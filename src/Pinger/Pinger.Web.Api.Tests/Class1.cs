819b174a0c855e9036af3f7529fb6f6975221940
singed <parfenchuks@gmail.com>
2013-09-04 18:14:28 +0300

    initial project ver

---
+++ src/Pinger/Pinger.Web.Api.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinger.Web.Api.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Pinger/Pinger.Web.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pinger.Web.Api.WebApiApplication" Language="C#" %>
