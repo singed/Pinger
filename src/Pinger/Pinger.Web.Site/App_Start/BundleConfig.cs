2cad16c55cb2599ac572239c5508c22c778b7b05
singed <parfenchuks@gmail.com>
2014-02-08 15:14:17 +0200

    Kepik icon on the map

---
+++ src/Pinger/Pinger.Web.Site/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace Pinger.Web.Site
+{
+    public static class BundleConfig 
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // scripts 
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-2.1.0.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts/").Include("~/Scripts/modules/common.js"));
+            // css
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles/common.css"));
+
+            //BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ src/Pinger/Pinger.Web.Site/Scripts/modules/common.js
@@ -0,0 +1,21 @@
+﻿function initialize(position) {
+    var pyrmont = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var mapOptions = {
+        center: pyrmont,
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+
+    };
+
+    var image = '/Content/Images/ladybug-icon.png';
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+        map: map,
+        icon: image
+});
+}
+google.maps.event.addDomListener(window, 'load', function () {
+    var position = navigator.geolocation.getCurrentPosition(initialize);
+});
